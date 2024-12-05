45ca4a9a9262acb536f0571b9d441738f34ae997
Jorge Vejarano <jorgevejarano@gmail.comm>
2024-12-05 10:02:33 -0400

    mostrar la última actualización de los datos

---
+++ js/app.js
@@ -0,0 +1,15 @@
+async function obtenerCotizacion() {
+    try {
+        const respuesta = await fetch('https://bo.dolarapi.com/v1/dolares/binance');
+        if (!respuesta.ok) {
+            throw new Error('Error al obtener la cotización');
+        }
+        const data = await respuesta.json();
+        document.getElementById('cotizacion').innerHTML = `<p>Compra: ${data.compra} Bs</p><p>Venta: ${data.venta} Bs</p>`;
+    } catch (error) {
+        document.getElementById('cotizacion').innerHTML = '<p>Error al obtener la cotización. Intente nuevamente más tarde.</p>';
+        console.error(error);
+    }
+}
+
+window.onload = obtenerCotizacion;
